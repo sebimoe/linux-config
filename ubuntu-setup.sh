49ba5360d7b8df6dd8212eb9a65bd0fb969aa8b0
sebimoe <sebi@sebi.moe>
2019-10-12 15:53:26 +0200

    Fixed apt install command

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -5,9 +5,9 @@ echo "setup-ubuntu.sh: starting"
 echo "setup-ubuntu.sh: apt update"
 apt update
 
-PACKAGES=htop nano git curl wget
+PACKAGES="htop nano git curl wget"
 echo "setup-ubuntu.sh: apt install $PACKAGES"
-apt install -y $PACKAGES
+apt install -y "$PACKAGES"
 
 echo "setup-ubuntu.sh: download zsh"
 setup-scripts/download-zsh

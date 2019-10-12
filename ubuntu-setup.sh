a565f44cecd892fd870f9ccb69566bda0330bed9
sebimoe <sebi@sebi.moe>
2019-10-12 13:26:13 +0200

    ubuntu-setup: added some packages

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -2,6 +2,13 @@
 
 echo "setup-ubuntu.sh: starting"
 
+echo "setup-ubuntu.sh: apt update"
+apt update
+
+PACKAGES=htop nano git curl wget
+echo "setup-ubuntu.sh: apt install $PACKAGES"
+apt install -y $PACKAGES
+
 echo "setup-ubuntu.sh: download zsh"
 setup-scripts/download-zsh
 
